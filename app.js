e485e8582ee1b4eb32a49d3e9f22b09413bad814
Ming Zhe Tan <tanmingzhe.99@gmail.com>
2022-02-18 15:27:06 +0800

    test

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 
+
+//Simple request time logger
+app.use(function(req, res, next){
+   console.log("A new request received at " + Date.now());
+   
+   //This function call is very important. It tells that more processing is
+   //required for the current request and is in the next middleware
+   //function route handler.
+   next();
+});
+
 app.get('/', function(req, res){
    res.send("Hello world!");
 });
@@ -9,4 +20,4 @@ app.get('/:id/:type', function(req, res){
    res.send(req.params.id + " " + req.params.type);
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT);
