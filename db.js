d6de6c890bbed722b2a3b6cc02589f3a57b3ef2f
Ming Zhe Tan <tanmingzhe.99@gmail.com>
2022-02-25 13:06:18 +0800

    test

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const dbConfig = require("./config/dbConfig.js");
+const dbConfig = require("../config/dbConfig.js");
 const db = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
