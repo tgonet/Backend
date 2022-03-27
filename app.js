d603e4d94bf7adb7cffd1e26c46dc47a044f8b57
tgonet <tanmingzhe.99@gmail.com>
2022-03-27 23:17:35 +0800

    bump

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ app.get('/', function(req, res){
       console.log("Friday:" + x + (last - day));
    }
    else{
-      diff1 = x.getDate() - day + (day == 0 ? -6:1);
-      diff2 = x.getDate() - day + (day == 0 ? -2:5);
+      diff1 = x - day + (day == 0 ? -6:1);
+      diff2 = x - day + (day == 0 ? -2:5);
       console.log("Monday:" + diff1);
       console.log("Friday:" + diff2);
    }
